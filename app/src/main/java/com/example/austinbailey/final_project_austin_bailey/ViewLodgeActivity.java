d0ee5474572223a6fa0077e04b85494e5f925f9a
Austin Bailey <baileyau@bc.edu>
2018-12-10 20:24:05 -0500

    Added Maps Activity and view Lodge info

--- app/src/main/java/com/example/austinbailey/final_project_austin_bailey/ViewLodgeActivity.java
+++ app/src/main/java/com/example/austinbailey/final_project_austin_bailey/ViewLodgeActivity.java
@@ -5,20 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class ViewLodgeActivity extends AppCompatActivity {
 
+    private Lodge lodge;
+    private TextView name, rating, address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_lodge);
 
-        Lodge lodge = (Lodge) getIntent().getSerializableExtra("lodge");
-        Log.i("MSG", lodge.name);
+        this.lodge = (Lodge) getIntent().getSerializableExtra("lodge");
+
+        this.name = findViewById(R.id.name);
+        this.rating = findViewById(R.id.rating);
+        this.address = findViewById(R.id.address);
+
+        this.name.setText(this.lodge.name);
+        this.rating.setText("Rating: " + Double.toString(this.lodge.rating));
+        this.address.setText(this.lodge.address);
     }
 
     public void map(View v) {
-
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra("lat", this.lodge.latitude);
+        intent.putExtra("lng", this.lodge.longitude);
+        intent.putExtra("name", this.lodge.name);
+        startActivity(intent);
     }
 
     public void save(View v) {
